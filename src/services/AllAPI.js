b920dda367a4598eedf17c28355c42229cb2e15c
Goutham1102 <krgouthamkrishna02@gmail.com>
2025-11-01 19:37:53 +0530

    updated again

--- src/services/AllAPI.js
+++ src/services/AllAPI.js
@@ -1,25 +1,22 @@
 import CommonAPI from "./CommonAPI";
 import { serverURL } from "./serverURL";
 
-
-
-// Add a new post;
-export const addPostAPI = async (postData)=>{
-    return await CommonAPI("POST",`${serverURL}/posts`,postData)
+// Add a new post
+export const addPostAPI = async (postData) => {
+    return await CommonAPI("POST", `${serverURL}/posts`, postData)
 };
 
 // Get all posts
-export const getAllPostsAPI =async()=>{
-    return await CommonAPI ("GET",`${serverURL}/posts`, '')
+export const getAllPostsAPI = async () => {
+    return await CommonAPI("GET", `${serverURL}/posts`, '')
 };
 
 // Delete a post by ID
-export const deletePostAPI = async(id)=>{
-    return await CommonAPI("DELETE", `${serverURL}/posts/${id}`,{})
+export const deletePostAPI = async (id) => {
+    return await CommonAPI("DELETE", `${serverURL}/posts/${id}`, {})
 };
 
 // Update a post by ID
-export const updatePostAPI = async(id,updatedData)=>{
-    return await CommonAPI("PUT",`${serverURL}/posts/${id}`,updatedData)
+export const updatePostAPI = async (id, updatedData) => {
+    return await CommonAPI("PUT", `${serverURL}/posts/${id}`, updatedData)
 };
-
