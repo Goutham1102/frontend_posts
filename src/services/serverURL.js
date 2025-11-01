769a42de89d2a9e3949611aaa69d5ad655d9331d
Goutham1102 <krgouthamkrishna02@gmail.com>
2025-11-01 18:54:32 +0530

    server updated

--- src/services/serverURL.js
+++ src/services/serverURL.js
@@ -1,4 +1,4 @@
 // export const serverURL = "http://localhost:3000/";
-export const serverURL = "http://localhost:3000";
-// export const serverURL = "https://post-server-mcs2.onrender.com";
+// export const serverURL = "http://localhost:3000";
+export const serverURL = "https://post-server-3-digp.onrender.com";
 
