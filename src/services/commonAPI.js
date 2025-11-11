2bb0e966beb47473e099c746fa7a5f36b6be98db
Goutham1102 <krgouthamkrishna02@gmail.com>
2025-11-11 20:57:25 +0530

    updatedcode

--- src/services/commonAPI.js
+++ src/services/commonAPI.js
@@ -1,16 +1,16 @@
-import axios from 'axios';
-const CommonAPI = async (httpMethod,url,reqbody) => {
-    const reqconfig ={
-        method:httpMethod,
-        url,
-        data :reqbody
-    }
-    //api call using axios
-    return await axios(reqconfig).then(res=>{
-        return res
-    })
-    .catch(err=>{
-        return err
-    })
-}
-export default CommonAPI
+ import axios from "axios";
+ const commonAPI = async(httpMethod, url,reqBody)=>{
+        const reqConfig ={
+            method:httpMethod,
+            url,
+            data : reqBody
+        }
+        //API call using axios
+        return await axios(reqConfig).then(res=>{
+            return res
+        })
+        .catch(err=>{
+            return err
+        })
+ }
+ export  default commonAPI
